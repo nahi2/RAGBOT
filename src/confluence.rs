0a8c3aee0d03803b44602c4c3481c52d7221bc7c
Nahiyan <nahiyan0667@gmail.com>
2024-05-26 04:32:49 -0700

    set creds

--- src/confluence.rs
+++ src/confluence.rs
@@ -1,1 +1,39 @@
+use dotenv::dotenv;
+use std::env;
 
+#[derive(Debug)]
+pub struct ConfCreds {
+    domain: String,
+    username: String,
+    password: String,
+}
+
+impl ConfCreds {
+    pub fn set_creds() -> Result<Self, String> {
+        dotenv().ok();
+
+        let domain = env::var("DOMAIN").map_err(|_| "Domain not set.".to_string())?;
+        let username = env::var("CONFLUENCE_USERNAME").map_err(|_| "Username not set.".to_string())?;
+        let password = env::var("CONFLUENCE_TOKEN").map_err(|_| "Password not set.".to_string())?;
+
+        Ok(ConfCreds {
+            domain,
+            username,
+            password,
+        })
+    }
+
+    pub fn get_domain(&self) -> &str {
+        &self.domain
+    }
+    pub fn get_username(&self) -> &str {
+        &self.username
+    }
+    pub fn get_password(&self) -> &str {
+        &self.password
+    }
+
+    pub fn get_pages(&self) -> Result<(),()> {
+
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,29 @@ use mongodb::{
     // Collection
 };
 use std::result;
+use crate::confluence::ConfCreds;
 
 type Result<T> = result::Result<T, ()>;
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    let conf_creds = match ConfCreds::set_creds() {
+        Ok(creds) => {
+            creds
+        }
+        Err(e) => {
+            eprintln!("{}", e);
+            return Ok(())
+        }
+    };
+
+    println!("creds: {:?}", conf_creds);
     let _ = Client::with_uri_str("mongdb://localhost:27017")
         .await
         .map_err(|e| {
             eprintln!("Failed to create client: {:?}", e);
         })?;
+
+
     Ok(())
 }
